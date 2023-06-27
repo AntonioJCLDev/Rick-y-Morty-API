0e2bf61fa719ab3f50b521914a9f9f8b60445468
Antonio.dev <antoniojcl.dev@gmail.com>
2023-06-27 17:46:37 +0100

    Funcionalidad básica

---
+++ index.js
@@ -0,0 +1,65 @@
+const url = fetch("https://rickandmortyapi.com/api/character");
+const search = document.getElementById("searchbar")
+
+
+
+function getCharacters(done){
+
+    const results = url
+
+    results
+        .then(res => res.json())
+        .then(data => {
+            done(data)
+        });
+}
+
+getCharacters(data => {
+    
+    data.results.forEach((personaje) => {
+        
+        const article = document.createRange().createContextualFragment(`
+        <article class = "article">
+            <div class="image-container">
+                <img src="${personaje.image}" alt="personaje">
+            </div>
+            <h2 id = "name">${personaje.name}</h2>
+            <p><span class="span-status">${personaje.status}</span></p>
+        </article>
+        `);
+
+        const main = document.querySelector("main");
+
+        main.append(article);
+    });
+});
+
+function getPages(){
+    console.log("Paco")
+    const info = url
+
+    info
+        .then(res => res.json())
+        .then(data => {
+            done(data)
+        });
+    
+    data.info.next
+}
+
+
+//Barra de busqueda fallida
+/* function searchCharacter() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let article = document.getElementsByClassName('article');
+      
+    for (i = 0; i < article.length; i++) { 
+        if (article[i].innerHTML.toLowerCase().includes(input)) {
+            article[i].style.display="none";
+        }
+        else {
+            article[i].style.display = article;                 
+        }
+    }
+} */
