ede49fdbe1e351e83494bbd6cff5d2393ae87184
arodriguezsym <96234108+arodriguezsym@users.noreply.github.com>
2023-06-27 20:53:27 +0100

    Change the js functionality

--- index.js
+++ index.js
@@ -1,64 +1,33 @@
-const url = fetch("https://rickandmortyapi.com/api/character");
 const search = document.getElementById("searchbar");
 const estructura = document.querySelector("#estructura");
-const botonAtrasDOM = document.querySelector("#atras");
-const infoPagDOM = document.querySelector("#info-pag");
-const botonSiguiente = document.querySelector("#siguiente");
-const elementosPorPagina = 20
-let paginaActual = 1;
-const baseDeDatos = url;
+const prevPage = document.querySelector("#atras");
+const infoPageDOM = document.querySelector("#info-pag");
+const nextPage = document.querySelector("#siguiente");
+const elsByPage = 20;
+let currentPage = 1;
 
-function avanzarPagina() {
-    paginaActual = paginaActual + 1;
-    renderizar();
+function getAllCharacters(currentPage) {
+	const page = "?page=" + currentPage
+    fetch("https://rickandmortyapi.com/api/character" + page)
+		.then(res => res.json())
+		.then(data => render(data))
 }
 
-function retrocederPagina() {
-    paginaActual = paginaActual - 1;
-    renderizar();
+function showNextPage() {
+    currentPage += 1;
+	getAllCharacters(currentPage)
 }
 
-function obtenerRebanadaDeBaseDeDatos(pagina = 1) {
-	const corteDeInicio = (paginaActual - 1) * elementosPorPagina;
-	const corteDeFinal = corteDeInicio + elementosPorPagina;
-	return baseDeDatos.slice(corteDeInicio, corteDeFinal);
+function showPrevPage() {
+	if(currentPage > 1){
+		currentPage -= 1
+		getAllCharacters(currentPage)		
+	}
 }
 
-function obtenerPaginasTotales() {
-    console.log(baseDeDatos.then(response => response.json()))
-    console.log(baseDeDatos.then(response => response.json()).length)
-	return Math.ceil(baseDeDatos.then(response => response.json()).length / elementosPorPagina);
-}
-
-function gestionarBotones() {
-    if (paginaActual === 1) {
-        botonAtrasDOM.setAttribute("disabled", true);
-    } else{
-        botonAtrasDOM.removeAttribute("disabled");
-    }
-
-
-
-
-}
-
-
-
-function getCharacters(done){
-
-    const results = url
-
-    results
-        .then(res => res.json())
-        .then(data => {
-            done(data)
-        });
-}
-
-getCharacters(data => {
-    
+function render(data){
+	document.querySelector("#estructura").innerHTML = ""
     data.results.forEach((personaje) => {
-        
         const article = document.createRange().createContextualFragment(`
         <article class = "article">
             <div class="image-container">
@@ -68,12 +37,9 @@ getCharacters(data => {
             <p><span class="span-status">${personaje.status}</span></p>
         </article>
         `);
-
         const main = document.querySelector("main");
-
         main.append(article);
     });
-});
+}
 
-
-botonAtrasDOM.addEventListener("click", obtenerPaginasTotales())
+getAllCharacters(currentPage)
